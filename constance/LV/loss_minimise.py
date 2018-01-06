1064b3d215947b4a672944f1f4d3daccecc17593
Constance Crozier <Constance@Constances-MacBook-Pro-3.local>
2018-01-06 19:39:41 +0000

    no message

--- constance/LV/loss_minimise.py
+++ constance/LV/loss_minimise.py
@@ -1,21 +1,21 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 from cvxopt import matrix, spdiag, sparse, solvers
 import random
 
-Pmax = 7 # kW
-x0 = 1.0 # kW
-pph = 1
+Pmax = 70 # kW
+x0 = 0.0 # kW
+pph = 6
 T = 24*pph
+
+skipUnused = False
+unused = []
 '''
 NOTES
 
-I need to EITHER incorporate the household profiles into the decision variable
-or I need to linearize around the actual operating point
-
-Ahhh decisions
-
-Also I need to decide if the negative values of c are actually wrong?
+Can i alter the optimisation to skip households where vehicles do not
+need charging?
 
 Also are the loads specified in kW or W?
 
@@ -54,6 +54,10 @@ with open('vehicle_demand_pool.csv','rU') as csvfile:
         profile = []
         for i in range(len(row)):
             profile.append(float(row[i]))
+        if skipUnused == True:
+            if sum(profile) == 0:
+                continue
+
         vehicle_profiles.append(profile)
         
 chosenV = []
@@ -69,19 +73,25 @@ avaliable = [] # contains range for which charging NOT allowed
 
 for v in range(55):
     energyV.append(sum(vehicle_profiles[chosenV[v]])/60)
-    energyHH.append(sum(household_profiles[chosen[v]])/60)
+    hh_en = 0
+    for t in range(0,1440,int(60/pph)):
+        hh_en += household_profiles[chosen[v]][t]
+    energyHH.append(hh_en/pph)
 
     if energyV[-1] == 0:
         i = 0
+        unused.append(v)
+        
     else:
         i = 0
         while vehicle_profiles[chosenV[v]][i] != 0:
             i += 1
+            
         while vehicle_profiles[chosenV[v]][i] == 0 and i < 1439:
             i += 1
 
     # picks random start time between 7 and 11 and gets actual end time
-    avaliable.append([int(7*pph+random.random()*4*pph),int(i*pph/60)+1])
+    avaliable.append([int(6*60+random.random()*int((i/60)-6)*60),i])
 
 c = []
 with open('c.csv','rU') as csvfile:
@@ -99,7 +109,7 @@ A = matrix(0.0,(110,55*T))
 b = matrix(0.0,(110+110*T,1))
 
 for v in range(55):
-    b[2*v] = energyV[v]+energyHH[v]-x0*24
+    b[2*v] = energyV[v]+energyHH[v]-x0*24 # energy required in kW
     b[2*v+1] = -energyV[v]-energyHH[v]+x0*24
     
     for t in range(T):
@@ -110,24 +120,105 @@ A = sparse([A,spdiag([-1]*55*T),spdiag([1]*55*T)])
 
 for i in range(55*T):
     hh = chosen[i%55]
-    t = i%T
-    b[110+i] = household_profiles[hh][t]-x0
+    t = int(int(i/55)*60/pph)
+    
+    b[110+i] = -household_profiles[hh][t]+x0
     
     if t > avaliable[i%55][0] and t < avaliable[i%55][1]: # if unavaliable
         b[110+55*T+i,0] = household_profiles[hh][t]-x0
     else:
         b[110+55*T+i,0] = household_profiles[hh][t]-x0+Pmax
 
-with open('A.csv','w') as csvfile:
-    writer = csv.writer(csvfile)
-    for i in range(A.size[0]):
-        row = []
-        for j in range(A.size[1]):
-            row.append(A[i,j])
-        writer.writerow(row)
+sol=solvers.lp(c,A,b)
+x = sol['x']
+lm = [0.0]*T
+bl = [0.0]*T
+for v in range(55):
+    for t in range(T):
+        lm[t] += x[55*t+v]/55
+        bl[t] += household_profiles[chosen[v]][int(t*60/pph)]
+
+# NOW EMBARKING ON THE LOAD FLATTENING FOR COMPARISON
+n = 55-len(unused)
+print(n)
+b = []
+
+for i in range(55):
+    if i in unused:
+        continue
+    b.append(energyV[i])
+    
+A1 = matrix(0.0,(n,T*n))
+A2 = matrix(0.0,(n,T*n))
+
+b += [0.0]*n
+b = matrix(b)
+
+skp = 0
+for j in range(55):
+    if j in unused:
+        skp += 1
+        continue
+
+    v = j-skp
+
+    for i in range(T):
+        A1[n*(T*v+i)+v] = 1.0/pph
+        
+        if i*60/pph > avaliable[j][0] and i*60/pph < avaliable[j][1]:
+            A2[n*(T*v+i)+v] = 1.0
+
+A = sparse([A1,A2])
+A3 = spdiag([-1]*(T*n))
+A4 = spdiag([1]*(T*n))
+G = sparse([A3,A4])
+
+h = []
+for i in range(T*n):
+    h.append(0.0)
+for i in range(T*n):
+    h.append(Pmax)
+    
+h = matrix(h)
+
+q = []
+for i in range(n):
+    q += bl
+
+q = matrix(q)
+
+I = spdiag([1]*T)
+P = sparse([[I]*n]*n)
+
 print(A.size)
 print(b.size)
-print(c.size)
-sol=solvers.lp(c,A,b)
+print(P.size)
+print(q.size)
+print(G.size)
+print(h.size)
 
-        
+sol = solvers.qp(P,q,G,h,A,b)
+x = sol['x']
+
+
+lf = [0.0]*T
+
+v = 0
+for i in range(55):
+    if i in unused:
+        continue
+
+    load = []
+    for t in range(T):
+        lf[t] += x[v*T+t]/55
+    v += 1
+
+for i in range(len(bl)):
+    bl[i] = bl[i]/55
+    lf[i] += bl[i]
+    
+plt.figure(1)
+plt.plot(bl)
+plt.plot(lm)
+plt.plot(lf)
+plt.show()        
