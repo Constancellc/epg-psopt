29ad99c757c9c282180a643d1939fa088ee12a10
Matt Deakin <matthew.deakin@eng.ox.ac.uk>
2019-01-23 18:21:15 +0000

    Solar data plots completed to show boss and parameters found.

--- ptech18/datasets/plot_california_pv.py
+++ ptech18/datasets/plot_california_pv.py
@@ -7,20 +7,23 @@ from math import gamma
 
 if getpass.getuser()=='chri3793':
     fn = r"C:\Users\chri3793\Documents\MATLAB\DPhil\epg-psopt\ptech18\datasets\california_residential_pv.csv"
-
+elif getpass.getuser()=='Matt':
+    sn = r"C:\Users\Matt\Desktop\wc190128\\"
+    fn = r"C:\Users\Matt\Documents\MATLAB\epg-psopt\ptech18\datasets\california_residential_pv.csv"
 QWE = pd.read_csv(fn)
 
 WER = QWE['Customer Sector']=='Residential'
 QWE = QWE[WER]
 
-RTY = QWE['System Size AC']
-RTY = RTY[RTY<20]
-RTY = RTY[RTY>0]
+pmax=20
 
+RTY = QWE['System Size AC']
+RTY = RTY[RTY<pmax]
+RTY = RTY[RTY>0]
 
 RTYnp = np.array(RTY)
 
-s = np.log(np.mean(RTYnp)) - sum(np.log(RTYnp))/len(RTYnp)
+s = np.log(np.mean(RTYnp)) - sum(np.log(RTYnp))/len(RTYnp) # see gamma distribution wiki
 
 k = ((3 - s) + np.sqrt( (s-3)**2 + 24*s ))/(12*s)
 th = np.mean(RTYnp)/k
@@ -36,4 +39,17 @@ X = np.linspace(0,20,len(histx))
 dX = X[1] - X[0]
 gX = (X**(k-1))*np.exp(-X/th)/(gamma(k)*(th**k))
 plt.step(histx,histy)
-plt.plot(X,gX); plt.show()
+plt.plot(X,gX)
+plt.legend(('CEC data','Gamma MLE approx.'))
+plt.xlabel('x = AC System Size (kW)')
+plt.ylabel('p(x)')
+plt.title('California Residential PV Sizes (Oct 18)')
+plt.xlim((0,pmax))
+plt.ylim((0,plt.ylim()[1]))
+plt.xticks(np.arange(0,pmax+2,2))
+plt.grid(True)
+plt.savefig(sn+"plot_california_pv.png")
+# plt.show()
+
+print("k (approx):",k)
+print("Theta (approx):",th)
--- ptech18/datasets/plot_uk_pv.py
+++ ptech18/datasets/plot_uk_pv.py
@@ -2,28 +2,79 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import getpass
 
-fn = "C:\Users\chri3793\Documents\MATLAB\DPhil\epg-psopt\ptech18\datasets\\fit_report_part1.csv"
+if getpass.getuser()=='chri3793':
+    fn = r"C:\Users\chri3793\Documents\MATLAB\DPhil\epg-psopt\ptech18\datasets\\fit_report_part1.csv"
+elif getpass.getuser()=='Matt':
+    sn = r"C:\Users\Matt\Desktop\wc190128\\"
+    fn = r"C:\Users\Matt\Documents\MATLAB\epg-psopt\ptech18\datasets\fit_report_part1.csv"
+    fn1 = r"C:\Users\Matt\Documents\MATLAB\epg-psopt\ptech18\datasets\fit_report_part1.csv"
+    fn2 = r"C:\Users\Matt\Documents\MATLAB\epg-psopt\ptech18\datasets\fit_report_part2.csv"
+    fn3 = r"C:\Users\Matt\Documents\MATLAB\epg-psopt\ptech18\datasets\fit_report_part3.csv"
 
-data = pd.read_csv(fn)
+data = pd.read_csv(fn,encoding="latin")
+# data = pd.read_csv(fn)
+data1 = pd.read_csv(fn1,encoding="latin")
+data2 = pd.read_csv(fn2,encoding="latin")
+data3 = pd.read_csv(fn3,encoding="latin")
 
-pmax = 10
+dataTot = pd.concat((data1,data2,data3),ignore_index=True)
+dataTot = pd.concat((data1,data2,data3))
+
+pmax = 20
+
+dataTot = dataTot[dataTot['Technology']=='Photovoltaic']
+dataTot = dataTot[dataTot['Installation type']=='Domestic']
+dataTot = dataTot[dataTot['Declared net capacity']<pmax]
+dataTot = dataTot[dataTot['Declared net capacity']>0]
 
 data = data[data['Technology']=='Photovoltaic']
 data = data[data['Installation type']=='Domestic']
 data = data[data['Declared net capacity']<pmax]
 data = data[data['Declared net capacity']>0]
 
-codes = data['Installation Postcode'].iloc[0:16]
-i = 1
-for code in codes:
-	pv_set = data[data['Installation Postcode']==code]['Declared net capacity']
-	ax = plt.subplot(4,4,i)
-	plt.hist(pv_set,bins=40)
-	i = i+1
-	ax.set_title(code)
-	ax.set_xlim([0,pmax])
+hist=plt.hist(data['Declared net capacity'],bins=200,range=(0,20),density=True)
+histTot=plt.hist(dataTot['Declared net capacity'],bins=200,range=(0,20),density=True)
+histTotIns=plt.hist(dataTot['Installed capacity'],bins=200,range=(0,20),density=True)
+
+plt.close()
+
+histx = hist[1][:-1]
+histy = hist[0]
+histxTot = histTot[1][:-1]
+histyTot = histTot[0]
+histxTotIns = histTotIns[1][:-1]
+histyTotIns = histTotIns[0]
+# plt.step(histx,histy)
+plt.step(histxTotIns,histyTotIns)
+plt.step(histxTot,histyTot)
+plt.title('UK Domestic PV FIT Sizes (Oct 18)')
+plt.xlabel('x = Capacity (kW)')
+plt.ylabel('p(x)')
+plt.grid(True)
+plt.xlim((0,pmax))
+plt.ylim((0,plt.ylim()[1]))
+plt.xticks(np.arange(0,22,2))
+plt.legend(('Installed Capacity','Declared Net Capacity'))
+
+# plt.savefig(sn+"plot_uk_pv.png")
 plt.show()
+
+
+# codes = data['Installation Postcode'].iloc[0:16]
+# i = 1
+# for code in codes:
+	# pv_set = data[data['Installation Postcode']==code]['Declared net capacity']
+	# ax = plt.subplot(4,4,i)
+	# plt.hist(pv_set,bins=40)
+	# i = i+1
+	# ax.set_title(code)
+	# ax.set_xlim([0,pmax])
+# plt.show()
+
+
+
 # plt.show()
 
 # NG2 = data[data['Installation Postcode']=='NG2 ']['Declared net capacity']
