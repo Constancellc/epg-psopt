78b4f3138721775c60b7cee428401ce97038e1c1
Constance <constancellc@gmail.com>
2019-01-29 17:03:24 +0000

    LV simulation working for new networks but predict_losses() seems buggy

--- constance/LV/test.py
+++ constance/LV/test.py
@@ -5,7 +5,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lv_optimization_new import LVTestFeeder
 
-network = LVTestFeeder('manc_models/024')
-network.set_households_NR('')
-network.set_evs_MEA('')
+network = LVTestFeeder('manc_models/1')
+network.set_households_NR('../../../Documents/netrev/TC2a/03-Dec-2013.csv')
+network.set_evs_MEA('../../../Documents/My_Electric_Avenue_Technical_Data/'+
+                    'constance/ST1charges/')
+b = network.get_feeder_load()
+network.uncontrolled()
+print(network.predict_losses)
+p = network.get_feeder_load()
+print(network.predict_losses)
+network.load_flatten()
+p2 = network.get_feeder_load()
+print(network.predict_losses)
+network.loss_minimise()
+p3 = network.get_feeder_load()
+print(network.predict_losses)
 
+plt.figure()
+plt.plot(b)
+plt.plot(p)
+plt.plot(p2)
+plt.plot(p3)
+plt.show()
