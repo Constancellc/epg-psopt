A bunch of changes - Code updated to 'early bindings' as per Davis' notice, which seems to have speeded up calculations by about a factor of 3. This requires syntax to be more correct (e.g. .command -> .Command) - All nominal cases have now been run and timed to check that speeds are in the right ballpark and if the linear model is giving sensible results (it seems to be)

ptech18/dss_python_funcs.py
@@ -55,10 +55,10 @@ def add_generators(DSSObj,genBuses,delta):
         DSSObj.ActiveCircuit.SetActiveBus(genBus)
         if not delta: # ie wye
             genName = genBus.replace('.','_')
-            genKV = str(DSSObj.ActiveCircuit.ActiveBus.kVbase)
-            DSSObj.Text.command='new generator.'+genName+' phases=1 bus1='+genBus+' kV='+genKV+' kW=0.5 pf=1.0 model=1 vminpu=0.33 vmaxpu=3.0 conn=wye'
+            genKV = str(DSSObj.ActiveCircuit.ActiveBus.kVBase)
+            DSSObj.Text.Command='new generator.'+genName+' phases=1 bus1='+genBus+' kV='+genKV+' kW=0.5 pf=1.0 model=1 vminpu=0.33 vmaxpu=3.0 conn=wye'
         elif delta:
-            genKV = str(DSSObj.ActiveCircuit.ActiveBus.kVbase*np.sqrt(3))
+            genKV = str(DSSObj.ActiveCircuit.ActiveBus.kVBase*np.sqrt(3))
             if genBus[-1]=='1':
                 genBuses = genBus+'.2'
             if genBus[-1]=='2':
@@ -66,7 +66,7 @@ def add_generators(DSSObj,genBuses,delta):
             if genBus[-1]=='3':
                 genBuses = genBus+'.1'
             genName = genBuses.replace('.','_')
-            DSSObj.Text.command='new generator.'+genName+' phases=1 bus1='+genBuses+' kV='+genKV+' kW=0.5 pf=1.0 model=1 vminpu=0.33 vmaxpu=3.0 conn=wye'
+            DSSObj.Text.Command='new generator.'+genName+' phases=1 bus1='+genBuses+' kV='+genKV+' kW=0.5 pf=1.0 model=1 vminpu=0.33 vmaxpu=3.0 conn=wye'
         genNames = genNames+[genName]
     return genNames
 
@@ -84,7 +84,7 @@ def ld_vals( DSSCircuit ):
     ii = DSSCircuit.FirstPCElement()
     S=[]; V=[]; I=[]; B=[]; D=[]; N=[]
     while ii!=0:
-        if DSSCircuit.ActiveElement.name[0:4].lower()=='load':
+        if DSSCircuit.ActiveElement.Name[0:4].lower()=='load':
             DSSCircuit.Loads.Name=DSSCircuit.ActiveElement.Name.split(sep='.')[1]
             S.append(tp_2_ar(DSSCircuit.ActiveElement.Powers))
             V.append(tp_2_ar(DSSCircuit.ActiveElement.Voltages))
@@ -98,7 +98,7 @@ def ld_vals( DSSCircuit ):
         ii=DSSCircuit.NextPCElement()
     jj = DSSCircuit.FirstPDElement()
     while jj!=0:
-        if DSSCircuit.ActiveElement.name[0:4].lower()=='capa':
+        if DSSCircuit.ActiveElement.Name[0:4].lower()=='capa':
             DSSCircuit.Capacitors.Name=DSSCircuit.ActiveElement.Name.split(sep='.')[1]
             S.append(tp_2_ar(DSSCircuit.ActiveElement.Powers))
             V.append(tp_2_ar(DSSCircuit.ActiveElement.Voltages))
@@ -212,7 +212,7 @@ def cpf_get_loads(DSSCircuit):
     while i!=0:
         SS[i]=DSSCircuit.Loads.kW + 1j*DSSCircuit.Loads.kvar
         BB[i]=DSSCircuit.Loads.Name
-        i=DSSCircuit.Loads.next
+        i=DSSCircuit.Loads.Next
     imax = DSSCircuit.Loads.Count
     j = DSSCircuit.Capacitors.First
     while j!=0:
@@ -232,8 +232,8 @@ def cpf_set_loads(DSSCircuit,BB,SS,k):
     j = DSSCircuit.Capacitors.First
     while j!=0:
         DSSCircuit.Capacitors.Name=BB[j+imax]
-        DSSCircuit.Capacitors.kVar=k*SS[j+imax].imag
-        j=DSSCircuit.Capacitors.next
+        DSSCircuit.Capacitors.kvar=k*SS[j+imax].imag
+        j=DSSCircuit.Capacitors.Next
     return
 
 def find_tap_pos(DSSCircuit):
@@ -252,9 +252,9 @@ def fix_tap_pos(DSSCircuit, TC_No):
 
         
 def create_tapped_ybus_very_slow( DSSObj,fn_y,TC_No0 ):
-    DSSObj.Text.command='Compile ('+fn_y+')'
+    DSSObj.Text.Command='Compile ('+fn_y+')'
     fix_tap_pos(DSSObj.ActiveCircuit, TC_No0)
-    DSSObj.Text.command='set controlmode=off'
+    DSSObj.Text.Command='set controlmode=off'
     DSSObj.ActiveCircuit.Solution.Solve()
     
     SysY = DSSObj.ActiveCircuit.SystemY
@@ -282,9 +282,9 @@ def create_tapped_ybus_very_slow( DSSObj,fn_y,TC_No0 ):
         
         
 def create_tapped_ybus_slow( DSSObj,fn_y,TC_No0 ):
-    DSSObj.Text.command='Compile ('+fn_y+')'
+    DSSObj.Text.Command='Compile ('+fn_y+')'
     fix_tap_pos(DSSObj.ActiveCircuit, TC_No0)
-    DSSObj.Text.command='set controlmode=off'
+    DSSObj.Text.Command='set controlmode=off'
     DSSObj.ActiveCircuit.Solution.Solve()
     Ybus0 = tp_2_ar(DSSObj.ActiveCircuit.SystemY)
     n = int(np.sqrt(len(Ybus0)))
@@ -295,9 +295,9 @@ def create_tapped_ybus_slow( DSSObj,fn_y,TC_No0 ):
     return Ybus, YNodeOrder
 
 def create_tapped_ybus( DSSObj,fn_y,fn_ckt,TC_No0 ):
-    DSSObj.Text.command='Compile ('+fn_y+')'
+    DSSObj.Text.Command='Compile ('+fn_y+')'
     fix_tap_pos(DSSObj.ActiveCircuit, TC_No0)
-    DSSObj.Text.command='set controlmode=off'
+    DSSObj.Text.Command='set controlmode=off'
     DSSObj.ActiveCircuit.Solution.Solve()
     Ybus_,YNodeOrder_,n = build_y(DSSObj,fn_ckt)
     Ybus = Ybus_[3:,3:]
@@ -305,13 +305,13 @@ def create_tapped_ybus( DSSObj,fn_y,fn_ckt,TC_No0 ):
     return Ybus, YNodeOrder
 
 def build_y(DSSObj,fn_ckt):
-    # DSSObj.Text.command='Compile ('+fn_z+'.dss)'
+    # DSSObj.Text.Command='Compile ('+fn_z+'.dss)'
     YNodeOrder = DSSObj.ActiveCircuit.YNodeOrder
-    DSSObj.Text.command='show Y'
+    DSSObj.Text.Command='show Y'
     os.system("TASKKILL /F /IM notepad.exe")
     
-    fn_y = fn_ckt+'\\'+DSSObj.ActiveCircuit.name+'_SystemY.txt'
-    fn_csv = fn_ckt+'\\'+DSSObj.ActiveCircuit.name+'_SystemY_csv.txt'
+    fn_y = fn_ckt+'\\'+DSSObj.ActiveCircuit.Name+'_SystemY.txt'
+    fn_csv = fn_ckt+'\\'+DSSObj.ActiveCircuit.Name+'_SystemY_csv.txt'
 
     file_r = open(fn_y,'r')
     stream = file_r.read()
@@ -361,7 +361,7 @@ def get_idxs(e_idx,DSSCircuit,ELE):
                 except:
                     for ph in range(1,4):
                         e_idx.append(DSSCircuit.YNodeOrder.index(splt[0]+'.'+str(ph)))
-        i = ELE.next
+        i = ELE.Next
     return e_idx
 
 def get_element_idxs(DSSCircuit,ele_types):
@@ -375,7 +375,7 @@ def get_Yvbase(DSSCircuit):
     for yz in DSSCircuit.YNodeOrder:
         bus_id = yz.split('.')
         i = DSSCircuit.SetActiveBus(bus_id[0]) # return needed or this prints a number
-        Yvbase.append(1e3*DSSCircuit.ActiveBus.kvbase)
+        Yvbase.append(1e3*DSSCircuit.ActiveBus.kVBase)
     return np.array(Yvbase)
     
 def feeder_to_fn(WD,feeder):

ptech18/hcResults/pltHcResults.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 WD = os.path.dirname(sys.argv[0])
-feeders = ['13bus','34bus','123bus','epri5','epri7']
-feeders = ['epriJ1','epriK1','epriM1','epri5','epri24']
+feeders = ['epriJ1','epriK1','epriM1','epri5','epri7','epri24','8500node']
+pLoad = 1000*np.array([11.6,12.74,15.67,16.3,19.3,28.8,12.05])
 
 rslts = {}
 
@@ -30,27 +30,34 @@ for rslt in rslts.values():
 x = np.arange(len(feeders))
 dx = 0.25
 
+p0 = np.array(p0)
+p0lin = np.array(p0lin)
+p10 = np.array(p10)
+p10lin = np.array(p10lin)
+
 
 plt.subplot(221)
-plt.bar(x-(dx/2),p0,width=dx,zorder=3)
-plt.bar(x+(dx/2),p0lin,width=dx,zorder=3)
+plt.bar(x-(dx/2),p0/pLoad,width=dx,zorder=3)
+plt.bar(x+(dx/2),p0lin/pLoad,width=dx,zorder=3)
 plt.title('P0')
 plt.legend(('OpenDSS','Linear Model'))
-plt.xticks(x,feeders)
+plt.xticks(x,feeders,rotation=90)
+plt.ylabel('Fraction of peak load')
 plt.grid(True,zorder=0)
 
 plt.subplot(222)
-plt.bar(x-(dx/2),p10,width=dx,zorder=3)
-plt.bar(x+(dx/2),p10lin,width=dx,zorder=3)
+plt.bar(x-(dx/2),p10/pLoad,width=dx,zorder=3)
+plt.bar(x+(dx/2),p10lin/pLoad,width=dx,zorder=3)
 plt.title('P10')
-plt.xticks(x,feeders)
+plt.xticks(x,feeders,rotation=90)
+plt.ylabel('Fraction of peak load')
 plt.grid(True,zorder=0)
 
 plt.subplot(223)
 plt.bar(x-(dx/2),k0,width=dx,zorder=3)
 plt.bar(x+(dx/2),k0lin,width=dx,zorder=3)
 plt.title('K0')
-plt.xticks(x,feeders)
+plt.xticks(x,feeders,rotation=90)
 plt.grid(True,zorder=0)
 
 plt.subplot(224)
@@ -59,6 +66,6 @@ plt.bar(x+(dx/2),k10lin,width=dx,zorder=3)
 plt.title('K10')
 plt.tight_layout()
 plt.grid(True,zorder=0)
-plt.xticks(x,feeders)
+plt.xticks(x,feeders,rotation=90)
 
 plt.show()

ptech18/linHcCalcs.py
@@ -23,13 +23,13 @@ import dss_stats_funcs as dsf
 WD = os.path.dirname(sys.argv[0])
 
 # CHOOSE Network
-fdr_i = 5
+fdr_i = 14
 fdrs = ['eulv','n1f1','n1f2','n1f3','n1f4','13bus','34bus','37bus','123bus','8500node','37busMod','13busRegMod3rg','13busRegModRx','13busModSng','usLv','123busMod','13busMod','epri5','epri7','epriJ1','epriK1','epriM1','epri24']
 feeder = fdrs[fdr_i]
 # feeder = '213'
 
 netModel=0 # none
-netModel=1 # ltc
+# netModel=1 # ltc
 # netModel=2 # fixed
 
 lp_taps='Lpt'
@@ -50,15 +50,15 @@ nMc = int(1e2)
 nMc = int(3e1)
 
 if netModel==0:
-    circuitK = {'13bus':4.8,'34bus':5.4,'123bus':3.0,'8500node':1.2,'epri5':2.4,'epri7':1.3,'epriJ1':1.2,'epriK1':1.2,'epriM1':1.5,'epri24':1.5}
+    circuitK = {'eulv':1.8,'usLv':5.0,'13bus':4.8,'34bus':5.4,'123bus':3.0,'8500node':1.2,'epri5':2.4,'epri7':2.0,'epriJ1':1.2,'epriK1':1.2,'epriM1':1.5,'epri24':1.5}
 elif netModel==1:
-    circuitK = {'13bus':7.2,'34bus':5.4,'123bus':3.6}
+    circuitK = {'13bus':6.0,'34bus':8.0,'123bus':3.6}
 elif netModel==2:
-    circuitK = {'8500node':1.2,'epriJ1':3.6,'epriK1':1.5,'epriM1':1.8,'epri24':1.5}
+    circuitK = {'8500node':1.5,'epriJ1':3.6,'epriK1':1.5,'epriM1':1.8,'epri24':1.5}
 
 # PDF options
 dMu = 0.01
-dMu = 0.025
+# dMu = 0.025
 mu_k = circuitK[feeder]*np.arange(dMu,1.0,dMu) # NB this is as a PERCENTAGE of the chosen nominal powers.
 
 pdfName = 'gamma'
@@ -99,7 +99,12 @@ ckt = get_ckt(WD,feeder)
 fn_ckt = ckt[0]
 fn = ckt[1]
 
+# 
+from win32com.client import makepy
+sys.argv=["makepy","OpenDSSEngine.DSS"]
+makepy.main()
 DSSObj = win32com.client.Dispatch("OpenDSSEngine.DSS")
+
 DSSText = DSSObj.Text
 DSSCircuit = DSSObj.ActiveCircuit
 DSSSolution = DSSCircuit.Solution
@@ -137,7 +142,7 @@ elif netModel>0:
     
     Ktot = np.concatenate((KyP,KdP),axis=1)
 
-KtotCheck = np.sum(Ktot==0,axis=1)!=Ktot.shape[1] # [can't remember what this is for...]
+KtotCheck = np.sum(Ktot==0,axis=1)!=Ktot.shape[1] # [this seems to get rid of fixed regulated buses]
 Ktot = Ktot[KtotCheck]
 b0 = b0[KtotCheck]
 vBase = vBase[KtotCheck]
@@ -154,8 +159,9 @@ dvBase = LMfix['vKvbase'] # NB: this is different to vBase for ltc/regulator mod
 KyPfix = Kyfix[:,:Kyfix.shape[1]//2]
 KdPfix = Kdfix[:,:Kdfix.shape[1]//2]
 Kfix = np.concatenate((KyPfix,KdPfix),axis=1)
-Kfix = Kfix[KtotCheck]
-
+KfixCheck = np.sum(Kfix==0,axis=1)!=Kfix.shape[1] # [can't remember what this is for...]
+Kfix = Kfix[KfixCheck]
+dvBase = dvBase[KfixCheck]
 
 # REDUCE THE LINEAR MODEL to a nice form for multiplication
 KtotPu = dsf.vmM(1/vBase,Ktot) # scale to be in pu
@@ -164,18 +170,18 @@ KfixPu = dsf.vmM(1/dvBase,Kfix)
 
 # OPENDSS ADMIN =======================================
 # B1. load the appropriate model/DSS
-DSSText.command='Compile ('+fn+'.dss)'
+DSSText.Command='Compile ('+fn+'.dss)'
 BB0,SS0 = cpf_get_loads(DSSCircuit)
 
 cpf_set_loads(DSSCircuit,BB0,SS0,load_point)
 DSSSolution.Solve()
 
 if not netModel:
-    DSSText.command='set controlmode=off'
+    DSSText.Command='set controlmode=off'
 elif netModel:
-    DSSText.command='set maxcontroliter=300'
+    DSSText.Command='set maxcontroliter=300'
     DSSObj.AllowForms=False
-DSSText.command='set maxiterations=100'
+DSSText.Command='set maxiterations=100'
 
 YNodeVnom = tp_2_ar(DSSCircuit.YNodeVarray)
 YZ = DSSCircuit.YNodeOrder
@@ -237,27 +243,28 @@ for i in range(pdfData['nP'][0]):
             dvOut = np.zeros((nMc,len(v_idx)))
             conv = []
             DVconv = []
+            print('\nRun:',jj,'/',pdfData['nP'][-1])
             for j in range(nMc):
                 if j%(nMc//4)==0:
                     print(j,'/',nMc)
                 set_generators( DSSCircuit,genNames,pdfGen[:,j]*pdfData['mu_k'][jj] )
                 
-                DSSText.command='Batchedit load..* vmin=0.33 vmax=3.0 model=1'
-                DSSText.command='Batchedit generator..* vmin=0.33 vmax=3.0'
-                DSSText.command='Batchedit regcontrol..* band=1.0' # seems to be as low as we can set without bit problems
+                DSSText.Command='Batchedit load..* vmin=0.33 vmax=3.0 model=1'
+                DSSText.Command='Batchedit generator..* vmin=0.33 vmax=3.0'
+                DSSText.Command='Batchedit regcontrol..* band=1.0' # seems to be as low as we can set without bit problems
                 
                 DSSSolution.Solve()
                 conv = conv+[DSSSolution.Converged]
                 v00 = tp_2_ar(DSSCircuit.YNodeVarray)
                 
-                DSSText.command='Batchedit generator..* kW=0.001'
-                DSSText.command='set controlmode=off'
+                DSSText.Command='Batchedit generator..* kW=0.001'
+                DSSText.Command='set controlmode=off'
                 DSSSolution.Solve()
 
                 DVconv = DVconv+[DSSSolution.Converged]
                 DV00 = tp_2_ar(DSSCircuit.YNodeVarray)
                 
-                DSSText.command='set controlmode=static'
+                DSSText.Command='set controlmode=static'
                 
                 vOut[j,:] = abs(v00)[3:][v_idx]/vBase
                 dvOut[j,:] = abs(abs(v00) - abs(DV00))[3:][v_idx]/vBase
@@ -370,12 +377,15 @@ if mcDssOn:
 if pltCns:
     fig, ax = plt.subplots()
     ax.set_prop_cycle(color=['red', 'blue', 'green'])
-    plt.plot(pdfData['mu_k'],Cns_pct_dss[0]);
+    if mcDssOn:
+        plt.plot(pdfData['mu_k'],Cns_pct_dss[0])
     plt.plot(pdfData['mu_k'],Cns_pct_lin[0],'--')
     plt.xlabel('Scale factor');
-    plt.ylabel('P(.)');
+    plt.ylabel('P(.), %');
     plt.title('Constraints')
     plt.legend(('Voltage deviation','Overvoltage','Undervoltage'))
+    if mcDssOn:
+        plt.savefig(os.path.join(SD,'pltCns.png'))
     plt.show()
 
 

ptech18/linearise_manc_py.py
@@ -8,11 +8,16 @@ from dss_python_funcs import *
 from dss_vlin_funcs import *
 
 WD = os.path.dirname(sys.argv[0])
+
+from win32com.client import makepy
+sys.argv=["makepy","OpenDSSEngine.DSS"]
+makepy.main()
 DSSObj = win32com.client.Dispatch("OpenDSSEngine.DSS")
+
 DSSText=DSSObj.Text
 DSSCircuit = DSSObj.ActiveCircuit
 DSSSolution=DSSCircuit.Solution
-DSSSolution.tolerance=1e-7
+DSSSolution.Tolerance=1e-7
 
 # ------------------------------------------------------------ circuit info
 test_model = True
@@ -20,11 +25,11 @@ test_model = False
 test_model_bus = True
 test_model_bus = False
 saveModel = True
-# saveModel = False
+saveModel = False
 saveCc = True
 saveCc = False
 verbose=True
-fdr_i = 22
+fdr_i = 20
 fig_loc=r"C:\Users\chri3793\Documents\DPhil\malcolm_updates\wc190117\\"
 fdrs = ['eulv','n1f1','n1f2','n1f3','n1f4','13bus','34bus','37bus','123bus','8500node','37busMod','13busRegMod3rg','13busRegModRx','13busModSng','usLv','123busMod','13busMod','epri5','epri7','epriJ1','epriK1','epriM1','epri24']; lp_taps='Nmt'
 # feeder='021'
@@ -62,8 +67,8 @@ DVslv_e=np.zeros([k.size,lin_points.size])
 for K in range(len(lin_points)):
     lin_point = lin_points[K]
     # run the dss
-    DSSText.command='Compile ('+fn+'.dss)'
-    DSSText.command='Batchedit load..* vminpu=0.33 vmaxpu=3'
+    DSSText.Command='Compile ('+fn+'.dss)'
+    DSSText.Command='Batchedit load..* vminpu=0.33 vmaxpu=3'
     BB00,SS00 = cpf_get_loads(DSSCircuit)
     if lp_taps=='Nmt':
         TC_No0 = find_tap_pos(DSSCircuit) # NB TC_bus is nominally fixed
@@ -81,15 +86,15 @@ for K in range(len(lin_points)):
     # print(np.log10(cndY))
     
     # CHECK 1: YNodeOrder 
-    DSSText.command='Compile ('+fn+'.dss)'
+    DSSText.Command='Compile ('+fn+'.dss)'
     YZ0 = DSSCircuit.YNodeOrder
     
     # Reproduce delta-y power flow eqns (1)
-    DSSText.command='Compile ('+fn+'.dss)'
+    DSSText.Command='Compile ('+fn+'.dss)'
 
     fix_tap_pos(DSSCircuit, TC_No0)
-    DSSText.command='Set Controlmode=off'
-    DSSText.command='Batchedit load..* vminpu=0.33 vmaxpu=3'
+    DSSText.Command='Set Controlmode=off'
+    DSSText.Command='Batchedit load..* vminpu=0.33 vmaxpu=3'
     DSSSolution.Solve()
     # BB00,SS00 = cpf_get_loads(DSSCircuit)
     
@@ -142,10 +147,10 @@ for K in range(len(lin_points)):
         print('\nVoltage error (lin point), Volts:',np.linalg.norm(Vh0-Vh)/np.linalg.norm(Vh))
         print('Voltage error (no load point), Volts:',np.linalg.norm(a-VnoLoad)/np.linalg.norm(VnoLoad),'\n')
     
-    DSSText.command='Compile ('+fn+')'
+    DSSText.Command='Compile ('+fn+')'
     fix_tap_pos(DSSCircuit, TC_No0)
-    DSSText.command='Set controlmode=off'
-    DSSText.command='Batchedit load..* vminpu=0.33 vmaxpu=3'
+    DSSText.Command='Set controlmode=off'
+    DSSText.Command='Batchedit load..* vminpu=0.33 vmaxpu=3'
 
     # NB!!! -3 required for models which have the first three elements chopped off!
     v_types = [DSSCircuit.Loads,DSSCircuit.Transformers,DSSCircuit.Generators]